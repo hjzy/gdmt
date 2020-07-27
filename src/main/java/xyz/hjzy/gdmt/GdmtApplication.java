a968a1631b85e5369b9b12cc16d3cb6e878fc330
yifan <2540584259@qq.com>
2020-07-27 09:37:54 +0800

    created

---
+++ src/main/java/xyz/hjzy/gdmt/GdmtApplication.java
@@ -0,0 +1,13 @@
+package xyz.hjzy.gdmt;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GdmtApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(GdmtApplication.class, args);
+    }
+
+}
---
+++ src/test/java/xyz/hjzy/gdmt/GdmtApplicationTests.java
@@ -0,0 +1,13 @@
+package xyz.hjzy.gdmt;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GdmtApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
