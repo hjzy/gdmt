72b6886e5b6af8c34a99ce1aa823e053b5ea071f
yifan <2540584259@qq.com>
2020-07-31 09:49:04 +0800

    页面能显示

--- src/main/java/com/train/gdmt/user/controller/UserControl.java
+++ src/main/java/com/train/gdmt/user/controller/UserControl.java
@@ -11,6 +11,6 @@ import org.springframework.web.bind.annotation.RequestMapping;
 public class UserControl {
     @RequestMapping("toLogin")
     public String toLogin(){
-        return "login-simple";
+        return "login";
     }
 }
